21a4d634e1544712d317ce300c942ab162a3a0f7
Vladik <hobtawlad@gmail.com>
2025-05-17 17:36:35 +0300

    nitial commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/search", async (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
